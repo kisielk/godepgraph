fd4bf528cdcfcf1af8ec1cc294fd8af2c610c30d
Kamil Kisiel <kamil@kamilkisiel.net>
2013-09-12 10:32:46 -0700

    Add more filtering flags

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
-godepgrah
-=========
+# godepgrah
 
 godepgraph is a program for generating a dependency graph of Go packages.
 
-Install
--------
+## Install
 
     go get github.com/kisielk/godepgraph
 
 
-Use
----
+## Use
 
 For basic usage, just give the package path of interest as the first
 argument:
@@ -25,9 +22,31 @@ graphviz tools installed you can render it by piping the output to dot:
 By default godepgraph will display packages in the standard library in the
 graph, though it will not delve in to their dependencies.
 
+## Ignoring Imports
+
+### The Go Standard Library
+
 If you want to ignore standard library packages entirely, use the -s flag:
 
     godepgraph -s github.com/kisielk/godepgraph
+
+### By Name
+
+Import paths can be included in a comma-separated list passed to the -i flag:
+
+    godepgraph -i github.com/foo/bar,github.com/baz/blah github.com/something/else
+
+The packages and their imports will be excluded from the graph, unless the imports
+are also imported by another package which is not excluded.
+
+### By Prefix
+
+Import paths can also be ignored by prefix. The -p flag takes a comma-separated
+list of prefixes:
+
+    godepgraph -p github.com,launchpad.net bitbucket.org/foo/bar
+
+
 
 Example
 -------
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"go/build"
 	"log"
 	"os"
+	"strings"
 )
 
 var (
@@ -16,21 +17,84 @@ var (
 	ignored = map[string]bool{
 		"C": true,
 	}
+	ignoredPrefixes []string
+
+	ignoreStdlib   = flag.Bool("s", false, "ignore packages in the go standard library")
+	ignorePrefixes = flag.String("p", "", "a comma-separated list of prefixes to ignore")
+	ignorePackages = flag.String("i", "", "a comma-separated list of packages to ignore")
 )
 
-func init() {
+func main() {
 	pkgs = make(map[string]*build.Package)
 	ids = make(map[string]int)
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 1 {
+		log.Fatal("need one package name to process")
+	}
+
+	if *ignorePrefixes != "" {
+		ignoredPrefixes = strings.Split(*ignorePrefixes, ",")
+	}
+	if *ignorePackages != "" {
+		for _, p := range strings.Split(*ignorePackages, ",") {
+			ignored[p] = true
+		}
+	}
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatalf("failed to get cwd: %s", err)
+	}
+	if err := processPackage(cwd, args[0]); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("digraph godep {")
+	for pkgName, pkg := range pkgs {
+		pkgId := getId(pkgName)
+
+		if isIgnored(pkg) {
+			continue
+		}
+
+		var color string
+		if pkg.Goroot {
+			color = "palegreen"
+		} else {
+			color = "paleturquoise"
+		}
+
+		fmt.Printf("%d [label=\"%s\" style=\"filled\" color=\"%s\"];\n", pkgId, pkgName, color)
+
+		// Don't render imports from packages in Goroot
+		if pkg.Goroot {
+			continue
+		}
+
+		for _, imp := range pkg.Imports {
+			impPkg := pkgs[imp]
+			if impPkg == nil || isIgnored(impPkg) {
+				continue
+			}
+
+			impId := getId(imp)
+			fmt.Printf("%d -> %d;\n", pkgId, impId)
+		}
+	}
+	fmt.Println("}")
 }
 
 func processPackage(root string, pkgName string) error {
-	if ignored[pkgName] {
-		return nil
-	}
-
 	pkg, err := build.Import(pkgName, root, 0)
 	if err != nil {
 		return fmt.Errorf("failed to import %s: %s", pkgName, err)
+	}
+
+	if isIgnored(pkg) {
+		return nil
 	}
 
 	pkgs[pkg.ImportPath] = pkg
@@ -60,58 +124,23 @@ func getId(name string) int {
 	return id
 }
 
-func main() {
-	ignoreStdlib := flag.Bool("s", false, "ignore packages in the go standard library")
-	flag.Parse()
-
-	args := flag.Args()
-
-	if len(args) != 1 {
-		log.Fatal("need one package name to process")
-	}
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Fatalf("failed to get cwd: %s", err)
-	}
-	if err := processPackage(cwd, args[0]); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("digraph godep {")
-	for pkgName, pkg := range pkgs {
-		pkgId := getId(pkgName)
-
-		var color string
-		if pkg.Goroot {
-			if !*ignoreStdlib {
-				color = "palegreen"
-			} else {
-				continue
-			}
-		} else {
-			color = "paleturquoise"
-		}
-
-		fmt.Printf("%d [label=\"%s\" style=\"filled\" color=\"%s\"];\n", pkgId, pkgName, color)
-
-		// Don't render imports from packages in Goroot
-		if pkg.Goroot {
-			continue
-		}
-
-		for _, imp := range pkg.Imports {
-			if ignored[imp] {
-				continue
-			}
-
-			impPkg := pkgs[imp]
-			if impPkg.Goroot && *ignoreStdlib {
-				continue
-			}
-			impId := getId(imp)
-			fmt.Printf("%d -> %d;\n", pkgId, impId)
+func hasPrefixes(s string, prefixes []string) bool {
+	for _, p := range prefixes {
+		if strings.HasPrefix(s, p) {
+			return true
 		}
 	}
-	fmt.Println("}")
+	return false
+}
+
+func isIgnored(pkg *build.Package) bool {
+	return ignored[pkg.ImportPath] || (pkg.Goroot && *ignoreStdlib) || hasPrefixes(pkg.ImportPath, ignoredPrefixes)
+}
+
+func debug(args ...interface{}) {
+	fmt.Fprintln(os.Stderr, args...)
+}
+
+func debugf(s string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, s, args...)
 }
