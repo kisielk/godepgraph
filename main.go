817ea78c90b60d075e4906da2673181308a90cc3
Kamil Kisiel <kamil@kamilkisiel.net>
2013-10-26 21:44:47 -0700

    Bail earlier if a package is in the ignored list.
    Fixes #1

--- main.go
+++ main.go
@@ -88,6 +88,10 @@ func main() {
 }
 
 func processPackage(root string, pkgName string) error {
+	if ignored[pkgName] {
+		return nil
+	}
+
 	pkg, err := build.Import(pkgName, root, 0)
 	if err != nil {
 		return fmt.Errorf("failed to import %s: %s", pkgName, err)
